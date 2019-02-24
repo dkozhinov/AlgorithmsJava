d60e2dfa41530a7db4586a7a0b44b0607612f2d7
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-02-25 00:12:32 +1100

    Выполнено Создать массив большого размера (миллион элементов) и Заполнить массив случайными числами.

--- src/Algorithms1/Lesson2Launcher.java
+++ src/Algorithms1/Lesson2Launcher.java
@@ -1,11 +1,22 @@
 package Algorithms1;
 
 /**
- * Java. Level 2. Lesson X. Homework.
+ * Java. Algoritms. Lesson 2. Homework.
  *
  * @author Dmitry Kozhinov d.kozhinov@mail.ru
  * Created on 24.02.2019
  */
 
+
+
+// 1. Создать массив большого размера (миллион элементов).
+// 2. Написать методы удаления, добавления, поиска элемента массива.
+// 3. Заполнить массив случайными числами.
+// 4. Написать методы, реализующие рассмотренные виды сортировок, и проверить скорость выполнения каждой.
 public class Lesson2Launcher {
+
+    public static void main(String[] args) {
+        MyArray myArray = new MyArray(1000000);
+
+    }
 }
--- src/Algorithms1/MyArray.java
+++ src/Algorithms1/MyArray.java
@@ -1,11 +1,36 @@
 package Algorithms1;
 
+import java.util.Random;
+
 /**
- * Java. Level 2. Lesson X. Homework.
+ * Java. Algoritms. Lesson 2. Homework.
  *
  * @author Dmitry Kozhinov d.kozhinov@mail.ru
  * Created on 24.02.2019
  */
 
+// 1. Создать массив большого размера (миллион элементов).
+// 2. Написать методы удаления, добавления, поиска элемента массива.
+// 3. Заполнить массив случайными числами.
+// 4. Написать методы, реализующие рассмотренные виды сортировок, и проверить скорость выполнения каждой.
 public class MyArray {
+
+    private int size;
+    private int[] myArrayInt;
+    private Random random;
+    private final int  RANGE_OF_RANDOM_NUMBERS = 10; // Размер диапазона для заполнения массива случайными числами [0-9]
+
+    public MyArray(int size) {
+        this.size = size;
+        myArrayInt = new int[size];
+        fillArray();
+    }
+
+    private void fillArray() {
+        random = new Random();
+        for(int i=0; i< size; i++) {
+            myArrayInt[i] = random.nextInt(RANGE_OF_RANDOM_NUMBERS);
+        }
+    }
+
 }
