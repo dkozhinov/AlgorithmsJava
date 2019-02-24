7b997947aaf0c980766f9a36320f7638a48a83c0
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-02-25 00:11:23 +1100

    Выполнено Создать массив большого размера (миллион элементов) и Заполнить массив случайными числами.

---
+++ src/Algorithms1/Lesson2Launcher.java
@@ -0,0 +1,11 @@
+package Algorithms1;
+
+/**
+ * Java. Level 2. Lesson X. Homework.
+ *
+ * @author Dmitry Kozhinov d.kozhinov@mail.ru
+ * Created on 24.02.2019
+ */
+
+public class Lesson2Launcher {
+}
